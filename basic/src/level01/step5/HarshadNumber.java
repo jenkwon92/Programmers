58cbde5eee56d07e7684e3886fb817b19e8bee51
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-08-14 01:21:32 +0900

    210814

---
+++ basic/src/level01/step5/HarshadNumber.java
@@ -0,0 +1,14 @@
+package level01.step5;
+
+import java.util.Scanner;
+
+public class HarshadNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		Solution sol = new Solution(x);
+		Array
+		System.out.println(sol);
+	}
+	
+}
