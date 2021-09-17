855677604a587b602319a1d42faa4cff7be90d1b
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-09-17 22:36:09 +0900

    210917

--- basic/src/level01_08_03_Dart/Solution.java
+++ basic/src/level01_08_03_Dart/Solution.java
@@ -1,5 +1,26 @@
 package level01_08_03_Dart;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Solution {
+	 public static int solution(String dartResult) {
+	        int answer = 0;
+	       ArrayList list = new ArrayList(dartResult.length());
+	        for(int i=0; i<list.size(); i++) {
+	        	list.add(dartResult.indexOf(i));
+	        	System.out.println(Arrays.toString());
+	        }
+	        return answer;
+	    }
+
+	 public static void main(String[] args) {
+		 /*
+		  점수는 0에서 10 사이의 정수이다.
+			보너스는 S, D, T 중 하나이다.
+			옵선은 *이나 # 중 하나이며, 없을 수도 있다.
+		  */
+		 String dartResult = "1S2D*3T";
+		 solution(dartResult);
+	}
 }
