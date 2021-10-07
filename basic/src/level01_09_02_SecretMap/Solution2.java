1ef6dd9c35fcbef99804b2db9bf8c5fc416f21a2
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-10-07 22:32:30 +0900

    211007

--- basic/src/level01_09_02_SecretMap/Solution2.java
+++ basic/src/level01_09_02_SecretMap/Solution2.java
@@ -3,17 +3,14 @@ package level01_09_02_SecretMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-class Solution {
+class Solution2 {
 	public String[] solution(int n, int[] arr1, int[] arr2) {
-		String[] answer = {};
-		// String binaryString = Integer.toBinaryString(i) //이진수 변환
+		String[] answer = new String[n];
+	
 		ArrayList<String> binaryStringArr1 = new ArrayList<String>();
 		ArrayList<String> binaryStringArr2 = new ArrayList<String>();
 		String zeroAdd = "";
-		String result = "";
-		ArrayList<String> list = new ArrayList<String>(n);
-		//String[] list = new String[n];
-		
+
 		for (int i = 0; i < n; i++) {
 			binaryStringArr1.add(Integer.toBinaryString(arr1[i]));
 			binaryStringArr2.add(Integer.toBinaryString(arr2[i]));
@@ -33,25 +30,27 @@ class Solution {
 				binaryStringArr2.set(i, zeroAdd + binaryStringArr2.get(i));
 				zeroAdd = "";
 			}
+			String result = "";
 			for (int a = 0; a < n; a++) {
-				if(binaryStringArr1.get(i).charAt(a)=='0' && binaryStringArr2.get(i).charAt(a)=='0') {
-					result +=" ";
-				}else {
-					result +="#";
+				if (binaryStringArr1.get(i).charAt(a) == '0' && binaryStringArr2.get(i).charAt(a) == '0') {
+					result += "2";
+				} else {
+					result += "#";
 				}
-				list.add(i, result); 
-				System.out.println(result.toString());
 			}
+			answer[i] += result;
 			result = "";
 		}
 		return answer;
 	}
 
+
+
 	public static void main(String[] args) {
 		int n = 5;
 		int[] arr1 = { 9, 20, 28, 18, 11 };
 		int[] arr2 = { 30, 1, 21, 17, 28 };
-		String[] answer = new Solution().solution(n, arr1, arr2);
+		String[] answer = new Solution2().solution(n, arr1, arr2);
 
 		System.out.println(Arrays.toString(answer));
 
